12a5188a9072bb901d04ddcdabeb52628f3f2eb4
Ian Liu <37123262+Ianyliu@users.noreply.github.com>
2024-02-24 18:03:32 +0000

    .

--- main.py
+++ main.py
@@ -9,44 +9,35 @@ from note_engine import note_engine
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
-from pdf import canada_engine, documents_engine
+from pdf import canada_engine, ky_engine
 
 
-# population_path = os.path.join("data", "population.csv")
-# population_df = pd.read_csv(population_path)
+population_path = os.path.join("data", "population.csv")
+population_df = pd.read_csv(population_path)
 
-# population_query_engine = PandasQueryEngine(
-#     df=population_df, verbose=True, instruction_str=instruction_str
-# )
-# population_query_engine.update_prompts({"pandas_prompt": new_prompt})
-
-# tools = [
-#     note_engine,
-#     QueryEngineTool(
-#         query_engine=population_query_engine,
-#         metadata=ToolMetadata(
-#             name="population_data",
-#             description="this gives information at the world population and demographics",
-#         ),
-#     ),
-#     QueryEngineTool(
-#         query_engine=canada_engine,
-#         metadata=ToolMetadata(
-#             name="canada_data",
-#             description="this gives detailed information about canada the country",
-#         ),
-#     ),
-# ]
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str
+)
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
 
 tools = [
+    note_engine,
     QueryEngineTool(
-        query_engine=documents_engine,
+        query_engine=population_query_engine,
         metadata=ToolMetadata(
-            name="chat data",
-            description="this gives detailed information about chats",
+            name="population_data",
+            description="this gives information at the world population and demographics",
+        ),
+    ),
+    QueryEngineTool(
+        query_engine=canada_engine,
+        metadata=ToolMetadata(
+            name="canada_data",
+            description="this gives detailed information about canada the country",
         ),
     ),
 ]
+
 
 llm = OpenAI(model="gpt-3.5-turbo")
 agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
--- pdf.py
+++ pdf.py
@@ -1,5 +1,4 @@
 import os
-# from llama_index.readers.file.pymu_pdf import PyMuPDFReader as PDFReader
 from llama_index.readers.file import PDFReader
 from llama_index.core.storage import StorageContext
 from llama_index.core import VectorStoreIndex
@@ -30,19 +29,20 @@ canada_index = get_index(canada_pdf, "canada")
 canada_engine = canada_index.as_query_engine()
 
 
-def get_doc_index(data, index_name):
-    index = None
-    if not os.path.exists(index_name):
-        print("building index", index_name)
-        index = VectorStoreIndex.from_documents(data, show_progress=True)
-        index.storage_context.persist(persist_dir=index_name)
-    else:
-        index = load_index_from_storage(
-            StorageContext.from_defaults(persist_dir=index_name)
-        )
-    
-    return index
 
-documents = SimpleDirectoryReader("/workspaces/PythonAgentAI/data").load_data()
-documents_index = get_doc_index(documents, "chat")
-documents_engine = documents_index.as_query_engine()
+# def get_doc_index(data, index_name):
+#     index = None
+#     if not os.path.exists(index_name):
+#         print("building index", index_name)
+#         index = VectorStoreIndex.from_documents(data, show_progress=True)
+#         index.storage_context.persist(persist_dir=index_name)
+#     else:
+#         index = load_index_from_storage(
+#             StorageContext.from_defaults(persist_dir=index_name)
+#         )
+    
+#     return index
+
+# documents = SimpleDirectoryReader("/workspaces/PythonAgentAI/data").load_data()
+# documents_index = get_doc_index(documents, "chat")
+# documents_engine = documents_index.as_query_engine()
